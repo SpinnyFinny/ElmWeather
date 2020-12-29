fc48afc6a67990007b85ef1dd8f5cb7ba1a5194d
SpinnyFinny <finn.hiltner@hotmail.com>
2020-12-28 17:19:02 -0800

    switched to elm ui

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,11 @@
 module Main exposing (..)
 
 import Browser
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
+import Element.Input as Input
 import Html exposing (Html, div, h1, img, span, text)
 import Html.Attributes exposing (class, src)
 import Http
@@ -31,9 +36,9 @@ type alias OpenWeatherResponse =
     {}
 
 
-currentForcastDecoder : Decoder CurrentForcast
-currentForcastDecoder =
-    Decode.map4 CurrentForcast
+currentForecastDecoder : Decoder CurrentForecast
+currentForecastDecoder =
+    Decode.map4 CurrentForecast
         (Decode.field "weather" <| Decode.index 0 <| Decode.field "icon" Decode.string)
         (Decode.at [ "main", "temp" ] Decode.float)
         (Decode.at [ "main", "temp_min" ] Decode.float)
@@ -45,16 +50,16 @@ currentForcastDecoder =
 
 
 type Msg
-    = GotCurrentWeather (Result Http.Error CurrentForcast)
+    = GotCurrentWeather (Result Http.Error CurrentForecast)
 
 
 type Status
-    = Loaded CurrentForcast
+    = Loaded CurrentForecast
     | Loading
     | Errored String
 
 
-type alias CurrentForcast =
+type alias CurrentForecast =
     { icon : String
     , currentTemp : Float
     , tempMin : Float
@@ -66,15 +71,15 @@ getCurrentWeather : Cmd Msg
 getCurrentWeather =
     Http.get
         { url = "https://api.openweathermap.org/data/2.5/weather?id=6090785&units=metric&appid=d2181649349acdfe3ca432ea262f0d7b"
-        , expect = Http.expectJson GotCurrentWeather currentForcastDecoder
+        , expect = Http.expectJson GotCurrentWeather currentForecastDecoder
         }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        GotCurrentWeather (Ok forcast) ->
-            ( { model | status = Loaded forcast }, Cmd.none )
+        GotCurrentWeather (Ok forecast) ->
+            ( { model | status = Loaded forecast }, Cmd.none )
 
         GotCurrentWeather (Err httpErr) ->
             ( { model | status = Errored "Leo fucked it up" }, Cmd.none )
@@ -86,29 +91,78 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div [ class "content" ] <|
-        case model.status of
-            Loaded forcast ->
-                viewCurrentWeather forcast model
+    case model.status of
+        Loaded forecast ->
+            -- viewCurrentWeather forecast model
+            Element.layout [] <| getCurrentForecast forecast
 
-            Loading ->
-                [ h1 [] [ text "Loading..." ] ]
+        Loading ->
+            Element.layout [] <|
+                Element.column
+                    [ centerX
+                    , centerY
+                    , Font.family
+                        [ Font.external
+                            { name = "Alata"
+                            , url = "https://fonts.googleapis.com/css?family=Alata"
+                            }
+                        , Font.sansSerif
+                        ]
+                    , Font.size
+                        40
+                    ]
+                    [ Element.text "Loading..." ]
 
-            Errored err ->
-                [ text ("Error: " ++ err) ]
+        Errored err ->
+            Html.text ("Error: " ++ err)
 
 
-viewCurrentWeather : CurrentForcast -> Model -> List (Html Msg)
-viewCurrentWeather forcast model =
-    [ div
-        [ class "current-weather" ]
-        [ h1 [] [ text "Current Weather in North Van" ]
-        , img [ class "weather-thumb", src ("http://openweathermap.org/img/w/" ++ forcast.icon ++ ".png") ] []
-        , div [ class "current-temp" ] [ text ("Current Temperture: " ++ String.fromFloat forcast.currentTemp ++ "℃") ]
-        , div [ class "current-high" ] [ text ("High: " ++ String.fromFloat forcast.tempMax ++ "℃") ]
-        , div [ class "current-low" ] [ text ("Low: " ++ String.fromFloat forcast.tempMin ++ "℃") ]
+getCurrentForecast : CurrentForecast -> Element msg
+getCurrentForecast forecast =
+    Element.column
+        [ centerX
+        , centerY
+
+        -- Background.gradient
+        -- { angle = 0.5
+        -- , steps =
+        --     [ rgb 0 0 0
+        --     , rgb 0 0 0
+        --     , rgb 0 0 0
+        --     , rgb 67 67 67
+        --     ]
+        -- }
+        , Background.color
+            (rgb 0 0 0)
+        , Border.rounded 7
+        , Font.color (rgb 1 1 1)
+        , Font.family
+            [ Font.external
+                { name = "Alata"
+                , url = "https://fonts.googleapis.com/css?family=Alata"
+                }
+            , Font.sansSerif
+            ]
+        , Element.padding 10
         ]
-    ]
+        [ Element.row [ width fill, spaceEvenly ]
+            [ Input.button [ Border.rounded 7, Border.width 1, padding 5 ] { onPress = Nothing, label = Element.text "North Vancouver" }
+            , Element.text "4:20"
+            ]
+        , Element.row [ centerX, centerY ]
+            [ Element.image []
+                { src = "http://openweathermap.org/img/w/" ++ forecast.icon ++ ".png", description = "" }
+            ]
+        , Element.row
+            [ width fill, spaceEvenly ]
+            [ Element.column []
+                [ Element.text ("High: " ++ String.fromFloat forecast.tempMax ++ "℃")
+                , Element.text ("Low: " ++ String.fromFloat forecast.tempMin ++ "℃")
+                ]
+            , Element.text
+                (String.fromFloat forecast.currentTemp ++ "℃")
+            ]
+        ]
 
 
 
